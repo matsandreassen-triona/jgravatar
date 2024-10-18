536c945c312c2fd6cff81d1a3cd821f14bcb1919
Mats Andreassen <mats.andreassen@triona.no>
2024-10-18 10:28:57 +0200

    Convert to Gradle project
    Currently, no longer shaded

--- src/test/java/com/timgroup/jgravatar/ImmutabilityTests.java
+++ src/test/java/com/timgroup/jgravatar/ImmutabilityTests.java
@@ -3,7 +3,7 @@ package com.timgroup.jgravatar;
 import static org.mutabilitydetector.unittesting.MutabilityAssert.assertInstancesOf;
 import static org.mutabilitydetector.unittesting.MutabilityMatchers.areImmutable;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class ImmutabilityTests {
 
@@ -11,13 +11,15 @@ public class ImmutabilityTests {
     public void gravatar() {
         assertInstancesOf(Gravatar.class, areImmutable());
     }
+
     @Test
     public void gravatarDefaultImage() {
         assertInstancesOf(GravatarDefaultImage.class, areImmutable());
     }
+
     @Test
     public void gravatarRating() {
         assertInstancesOf(GravatarRating.class, areImmutable());
     }
-    
+
 }
